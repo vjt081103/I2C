2be7033e913ee5192ae4b66ab8ad3847d531612a
vjt081103 <fananime2222@gmail.com>
2024-04-21 21:32:05 +0700

    main

---
+++ src/main.c
@@ -0,0 +1,204 @@
+#include "stm32f10x.h"
+#include "i2c_lcd.h"
+
+//delay function
+void Delay1Ms(void);
+void Delay_Ms(uint32_t u32DelayInMs);
+void delay_us(uint32_t delay);
+
+void Delay1Ms(void)
+{
+	
+	TIM_SetCounter(TIM2, 0);
+	while (TIM_GetCounter(TIM2) < 1000) {
+	}
+}
+
+void delay_us(uint32_t delay)
+{
+	
+	TIM_SetCounter(TIM2, 0);
+	while (TIM_GetCounter(TIM2) < delay) {
+	}
+}
+
+void Delay_Ms(uint32_t u32DelayInMs)
+{
+	
+	while (u32DelayInMs) {
+		Delay1Ms();
+		--u32DelayInMs;
+	}
+}
+
+//Timer init
+void TIM_INIT();
+
+void TIM_INIT() {
+	TIM_TimeBaseInitTypeDef timerInit;
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	
+	timerInit.TIM_CounterMode = TIM_CounterMode_Up;
+	timerInit.TIM_Period = 0xFFFF;
+	timerInit.TIM_Prescaler = 72 - 1;
+	
+	TIM_TimeBaseInit(TIM2, &timerInit);
+	
+	TIM_Cmd(TIM2, ENABLE);
+}
+
+
+//i2c function
+void i2c_init(void);
+void i2c_start(void);
+void i2c_stop(void);
+uint8_t i2c_write(uint8_t u8Data);
+uint8_t i2c_read(uint8_t u8Ack);
+
+#define SDA_0 GPIO_ResetBits(GPIOB, GPIO_Pin_0)
+#define SDA_1 GPIO_SetBits(GPIOB, GPIO_Pin_0)
+#define SCL_0 GPIO_ResetBits(GPIOB, GPIO_Pin_1)
+#define SCL_1 GPIO_SetBits(GPIOB, GPIO_Pin_1)
+#define SDA_VAL (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_0))
+
+void i2c_init(void)
+{
+	GPIO_InitTypeDef gpioInit;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	gpioInit.GPIO_Mode = GPIO_Mode_Out_OD;
+	gpioInit.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	gpioInit.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	GPIO_Init(GPIOB, &gpioInit);
+	
+	SDA_1;
+	SCL_1;
+}
+
+void i2c_start(void)
+{
+	
+	SCL_1;
+	delay_us(3);
+	SDA_1;
+	delay_us(3);
+	SDA_0;
+	delay_us(3);
+	SCL_0;
+	delay_us(3);
+}
+
+void i2c_stop(void)
+{
+	
+	SDA_0;
+	delay_us(3);
+	SCL_1;
+	delay_us(3);
+	SDA_1;
+	delay_us(3);
+}
+
+uint8_t i2c_write(uint8_t u8Data)
+{
+	uint8_t i;
+	uint8_t u8Ret;
+	
+	for (i = 0; i < 8; ++i) {
+		if (u8Data & 0x80) {
+			SDA_1;
+		} else {
+			SDA_0;
+		}
+		delay_us(3);
+		SCL_1;
+		delay_us(5);
+		SCL_0;
+		delay_us(2);
+		u8Data <<= 1;
+	}
+	
+	SDA_1;
+	delay_us(3);
+	SCL_1;
+	delay_us(3);
+	if (SDA_VAL) {
+		u8Ret = 0;
+	} else {
+		u8Ret = 1;
+	}
+	delay_us(2);
+	SCL_0;
+	delay_us(5);
+	
+	return u8Ret;
+}
+
+uint8_t i2c_read(uint8_t u8Ack)
+{
+	uint8_t i;
+	uint8_t u8Ret;
+	
+	SDA_1;
+	delay_us(3);
+	
+	for (i = 0; i < 8; ++i) {
+		u8Ret <<= 1;
+		SCL_1;
+		delay_us(3);
+		if (SDA_VAL) {
+			u8Ret |= 0x01;
+		}
+		delay_us(2);
+		SCL_0;
+		delay_us(5);
+	}
+	
+	if (u8Ack) {
+		SDA_0;
+	} else {
+		SDA_1;
+	}
+	delay_us(3);
+	
+	SCL_1;
+	delay_us(5);
+	SCL_0;
+	delay_us(5);
+	
+	return u8Ret;
+}
+
+// main
+int main(void)
+{
+	GPIO_InitTypeDef gpioInit;
+	
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	
+	
+	gpioInit.GPIO_Mode = GPIO_Mode_Out_PP;
+	gpioInit.GPIO_Pin = GPIO_Pin_13;
+	gpioInit.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	GPIO_Init(GPIOC, &gpioInit);
+	
+	TIM_INIT();
+	i2c_init();
+	
+	I2C_LCD_Clear();
+	I2C_LCD_Puts("STM32F103C8T6");
+	I2C_LCD_NewLine();
+	I2C_LCD_Puts("I2C LCD");
+		
+	while (1) {
+		GPIO_SetBits(GPIOC, GPIO_Pin_13);
+		Delay_Ms(500);
+		GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+		Delay_Ms(500);
+	}
+}
+
